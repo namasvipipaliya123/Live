0cbb274b8067e8c908b7f6b250411836ba3148d7
Namasvi <you@example.com>
2025-05-13 11:19:28 +0530

    Your commit message

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/users', userRoutes);
+app.use('/api', userRoutes);
 
 const PORT = process.env.PORT || 8090;
 app.listen(PORT, () => {
